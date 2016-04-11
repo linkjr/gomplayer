cefa4deb70eb85fcb12a1be3fd589bff0f017478
linkjr <linkjr@sina.com>
2016-04-12 01:15:00 +0800

    新增用户列表页面。

---
+++ GomPlayer.Web/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GomPlayer.IApplication;
+using GomPlayer.Infrastructure.Paging;
+
+namespace GomPlayer.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+
+        /// <summary>
+        /// 初始化 <c>UserController</c> 类的新实例。
+        /// </summary>
+        /// <param name="userService"></param>
+        public UserController(IUserService userService)
+        {
+            this._userService = userService;
+        }
+
+        // GET: User
+        public ActionResult Index(int id = 1)
+        {
+            var list = this._userService.List()
+                .ToPagedList(id, 10);
+            if (Request.IsAjaxRequest())
+                return PartialView("_ListPartial", list);
+            return View(list);
+        }
+    }
+}
